8e01f4c5dccfa307bbdb699f9551e2a243574e24
jsencinas <jesanchezencinas@gmail.com>
2026-09-05 18:02:21 -0600

    R11: programa en tres archivos con Makefile

---
+++ r11-multiarchivo/Punto.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Punto.h"
+
+Punto::Punto() : x(0), y(0){
+    std::cout << "Punto()\n";
+}
+
+Punto::Punto(int a) : x(a), y(a){
+    std::cout << "Punto(" << a << ")\n";
+}
+
+Punto::Punto(int a, int b) : x(a), y(b){
+    std::cout << "Punto(" << a << "," << b << ")\n";
+}
+
+Punto::~Punto() { 
+    std::cout << "~Punto(" << x << "," << y << ")\n";
+}
---
+++ r11-multiarchivo/main.cpp
@@ -0,0 +1,7 @@
+#include "Punto.h"
+
+int main(){
+    Punto a;          // Punto()
+    Punto b(5);       // Punto(5)
+    Punto c(1, 2);    // Punto(1,2)
+}
